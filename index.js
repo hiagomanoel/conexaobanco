cb00ee40c8418bed5dc223e1968a28f3e79c0ed6
hiagomanoel <162700610+hiagomanoel@users.noreply.github.com>
2025-05-27 09:59:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config(); 
+
+const db = require("./db");
+
+const port = process.env.PORT;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Rota para inserir clientes
+app.post("/client", async (req, res) => {
+    // O objeto req.body contem os dados enviados pelo cliente na requisição POST
+    await db.insertCustomers(req.body),
+
+    res.sendStatus(201);
+})
+
+app.listen(port);
+
+console.log("Backend Rodando!");
